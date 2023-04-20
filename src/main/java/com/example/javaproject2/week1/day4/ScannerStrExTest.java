a7fda6131f01b13498472e1113f91d0b15b8a836
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-04-20 09:56:42 +0900

    feat: ScannerStrEx, ScannerStrExTest 추가 - 스캐너 문자열 출력하기

---
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerStrExTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1.day4;
+
+import java.util.Scanner;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringsAndPlus();
+    }
+}
