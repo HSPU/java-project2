b06548ae8cfa971abc37969a14817cdcca37a5db
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-04-20 11:36:13 +0900

    feat: PrintWhy, PrintfEx, PrintTwoValuesShortage, MultiplicationTable 추가 - 제목과 내용 같음

---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintValuesShortage.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintValuesShortage {
+    public static void main(String[] args) {
+        System.out.printf("%s님", "신우석", "안녕하세요.");
+    }
+}
