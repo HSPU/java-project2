b6e5ca74e75c48cd7b4eaca5da8e98a7cea55d01
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-04-20 15:53:09 +0900

    feat: Codeup1014 ~ 1015 추가 - 1014 ~ 1015 문제풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1014.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String b = sc.next();
+        System.out.printf("%s %s", b, A);
+    }
+}
