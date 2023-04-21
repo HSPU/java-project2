a34ba2d8c8b4f972b32d85d736d72d1c08909cd4
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-04-21 15:26:15 +0900

    feat: User, UserTest 수정 - boolean 성인입니까?

--- src/main/java/com/example/javaproject2/week1/day5/UserTest.java
+++ src/main/java/com/example/javaproject2/week1/day5/UserTest.java
@@ -8,8 +8,11 @@ public class UserTest {
         wooseok.age = 27;
 
         User jaesung = new User();
-        jaesung.name = "재성 매니저님";
+        jaesung.name = "재성 매니저";
         jaesung.phoneNumber = "010-0000-0000";
         jaesung.age = 24;
+
+        System.out.printf("%s님은 성인입니까? %s\n", wooseok.name, wooseok.isAdult());
+        System.out.printf("%s님은 성인입니까? %s\n", jaesung.name, jaesung.isAdult());
     }
 }
