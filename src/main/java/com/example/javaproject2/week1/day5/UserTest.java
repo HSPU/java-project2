a2c2321072dc332921071252f6c581c20d71c671
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-04-21 15:23:30 +0900

    feat: User, UserTest 추가 - User를 클래스와 멤버 변수로 표현

---
+++ src/main/java/com/example/javaproject2/week1/day5/UserTest.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week1.day5;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User wooseok = new User();
+        wooseok.name = "신우석";
+        wooseok.phoneNumber = "010-1234-5678";
+        wooseok.age = 27;
+
+        User jaesung = new User();
+        jaesung.name = "재성 매니저님";
+        jaesung.phoneNumber = "010-0000-0000";
+        jaesung.age = 24;
+    }
+}
