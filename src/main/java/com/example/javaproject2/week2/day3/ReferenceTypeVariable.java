f5d9fe05d5e2349059a988958b3806ed8e9fd10f
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-04-26 09:46:08 +0900

    feat: ReferenceTypeArray, ReferenceTypeVariable, Student 추가 - 참조타입 배열 선언

---
+++ src/main/java/com/example/javaproject2/week2/day3/ReferenceTypeVariable.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.week2.day3;
+
+import com.example.javaproject2.PrintHello;
+
+public class ReferenceTypeVariable {
+    public static void main(String[] args) {
+        // Student 타입
+        Student student = new Student(); // new 를 써서 Student 객체를 인스턴스로
+
+        Student[] students = new Student[30];
+
+        Object obj = new Student();
+        Object obj2 = new PrintHello();
+
+        // primitive type 은 class 가 아님
+    }
+}
