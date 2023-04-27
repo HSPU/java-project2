5ba3c6555c05b5a37cb3247140cedc24150f4f77
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-04-27 13:18:52 +0900

    feat: Codeup1053 ~ 1058 추가 - Codeup 문제풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1053.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        if (num == 1) {
+            System.out.println("0");
+        } else {
+            System.out.println("1");
+        }
+    }
+}
