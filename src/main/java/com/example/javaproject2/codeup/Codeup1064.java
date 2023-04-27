2ffc26e6e57e06e433ce7ee1d57aa30281b6cb4c
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-04-27 22:07:47 +0900

    feat: Codeup1059 ~ 1064 추가 - Codeup 문제풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1064.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1064 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        int min = num1 < num2 ? num1 : num2;
+        min = min < num3 ? min : num3;
+        System.out.println(min);
+    }
+}
