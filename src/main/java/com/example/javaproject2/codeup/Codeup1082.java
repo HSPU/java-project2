b8f1c70933d7e04cf914b28e295687c1a1471e81
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-05-01 15:58:57 +0900

    feat: Codeup1081 ~ 1082 추가 - Codeup 문제풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1082.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1082 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String num = sc.next();
+        int hexVal = Integer.parseInt(num, 16);
+        for (int i = 1; i <= 15; i++) {
+            System.out.printf("%X*%X=%X\n", hexVal, i, hexVal * i);
+        }
+    }
+}
