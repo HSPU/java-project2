b7e89a39fe1bed9e7a25cc3ccf2cf3a607cc0965
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-05-01 13:18:34 +0900

    feat: ForLoopForwardStep, ForLoopForwardStep2 추가 - 10미만의 홀수 짝수 출력

---
+++ src/main/java/com/example/javaproject2/week3/ForLoopForwardStep2.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week3;
+
+public class ForLoopForwardStep2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i+=2) {
+            System.out.println(i); // 홀수만 출력
+        }
+    }
+}
