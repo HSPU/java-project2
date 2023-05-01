cd3eda9a38338adf44d8a1d7f7825f021465bf90
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-05-01 10:54:07 +0900

    feat: NewSwitchCaseSeason, NewSwitchCaseDaysOfMonth, SwitchCaseClinicHours, For0to9, ForLoopPrintHorizontal - 개선된 Switch 문, For 문

---
+++ src/main/java/com/example/javaproject2/week3/SwitchCaseClinicHours.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week3;
+
+public class SwitchCaseClinicHours {
+    public static void main(String[] args) {
+        String day = "월";
+        String time = "휴진";
+        switch(day) {
+            case "월", "화", "목", "금" -> time = "09:30-18:30";
+            case "토" -> time = "09:30-13:00";
+            case "수", "일" -> time = "휴진";
+            default -> System.out.println("해당하는 요일이 없습니다.");
+        }
+        System.out.printf("%s요일은 %s입니다.", day, time);
+    }
+}
