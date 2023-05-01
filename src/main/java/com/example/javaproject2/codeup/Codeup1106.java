24bd4548941eb406c24c9456fa4c98ccc1724ab1
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-05-01 10:51:35 +0900

    feat: Codeup1103, 1106 추가 - Codeup 문제풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1106.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.codeup;
+
+public class Codeup1106 {
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
