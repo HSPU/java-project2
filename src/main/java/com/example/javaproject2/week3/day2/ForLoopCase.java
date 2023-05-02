e3eb02e688dab7f27aafa839a4c3ee7dfe78915b
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-05-02 13:48:12 +0900

    feat: ForLoopCase, ForLoopCase2, Factorial 추가 - for 문 조건식 변경, 팩토리얼

---
+++ src/main/java/com/example/javaproject2/week3/day2/ForLoopCase.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week3.day2;
+
+public class ForLoopCase {
+    public static void main(String[] args) {
+        for (int i = 0; 2 * i <= 17 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
