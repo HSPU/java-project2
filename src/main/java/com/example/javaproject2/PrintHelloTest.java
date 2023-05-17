977b521055ad0aa84dab8dcd01eb1c40ebe73e4c
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-05-17 10:15:28 +0900

    feat: week4 ~ 5, Codeup, programmers 추가 - week5 day2까지

--- src/main/java/com/example/javaproject2/PrintHelloTest.java
+++
@@ -1,8 +0,0 @@
-package com.example.javaproject2;
-
-public class PrintHelloTest {
-    public static void main(String[] args) {
-        PrintHello printHello = new PrintHello(); // 인스턴스화
-        printHello.print();
-    }
-}
--- src/main/java/com/example/javaproject2/week1/ScannerExTest2.java
+++
@@ -1,8 +0,0 @@
-package com.example.javaproject2.week1;
-
-public class ScannerExTest2 {
-    public static void main(String[] args) {
-        ScannerEx se = new ScannerEx();
-        se.readTwoNumbersAndPlus();
-    }
-}
