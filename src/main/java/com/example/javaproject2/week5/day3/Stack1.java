ea8a2a9b95846b473379a9efe2035ffb850d63fc
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-05-17 21:58:07 +0900

    feat: Stack1 수정 - Stack1 완성

--- src/main/java/com/example/javaproject2/week5/day3/Stack1.java
+++ src/main/java/com/example/javaproject2/week5/day3/Stack1.java
@@ -6,6 +6,31 @@ public class Stack1 {
 
     public void push(int value) {
         arr[pointer++] = value;
-        System.out.println(pointer);
+    }
+
+    public int pop() {
+        if (isEmpty()) throw new RuntimeException("스택이 비었습니다.");
+        return arr[--pointer];
+    }
+
+    public boolean isEmpty() {
+        return pointer == 0;
+    }
+
+    public int peek() {
+        if (isEmpty()) throw new RuntimeException("스택이 비었습니다.");
+        return this.arr[pointer - 1];
+    }
+
+    public static void main(String[] args) {
+        Stack1 st = new Stack1();
+        st.push(10);
+        System.out.println(st.peek());
+        System.out.println(st.peek());
+        System.out.println(st.peek());
+        st.push(20);
+        System.out.println(st.peek());
+        st.pop();
+        System.out.println(st.peek());
     }
 }
